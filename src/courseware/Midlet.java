552a8d5dfae2905c635f7f1ac4244d620962f580
Daniel Ankomah <ankomah.d@gmail.com>
2014-02-10 15:40:06 +0000

    First commit

---
+++ src/courseware/Midlet.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package courseware;
+
+import javax.microedition.midlet.*;
+
+/**
+ * @author Daniel
+ */
+public class Midlet extends MIDlet {
+
+    public void startApp() {
+    }
+    
+    public void pauseApp() {
+    }
+    
+    public void destroyApp(boolean unconditional) {
+    }
+}
